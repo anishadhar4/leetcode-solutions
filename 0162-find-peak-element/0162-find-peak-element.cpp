cec45796bc020387a283a126ee0349a32aca06e8
anishadhar4 <anishadhar00@gmail.com>
2026-07-29 11:50:54 +0530

    Time: 0 ms (100%), Space: 12.7 MB (6.2%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -10,7 +10,7 @@ public:
         while(low<=high){
             int mid=low + (high-low)/2;
             if(nums[mid]>nums[mid-1]&& nums[mid]> nums[mid+1])return mid;
-            if(nums[mid]>nums[mid-1]){
+            if(nums[mid]>nums[mid-1]){//check on inc curve
                 low=mid+1;
             }else if(nums[mid]< nums[mid-1]){
                 high=mid-1;
