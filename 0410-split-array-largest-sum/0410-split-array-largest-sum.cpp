f28d51606f46543fc90b09af61607e6874b4a074
anishadhar4 <anishadhar00@gmail.com>
2026-07-31 14:36:03 +0530

    Time: 0 ms (100%), Space: 10.5 MB (73.84%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -15,6 +15,7 @@ private:
     }
 public:
     int splitArray(vector<int>& nums, int k) {
+        if(k>nums.size())return -1;
         int low = *max_element(nums.begin(),nums.end());
         int high=accumulate(nums.begin(),nums.end(),0);
         while(low<=high){
